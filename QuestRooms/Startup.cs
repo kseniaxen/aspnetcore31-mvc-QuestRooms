58c161fdfffa7cac51cdd8df3646819647c1409e
Ksenia Serdiuk <serdiuk.xen@gmail.com>
2020-10-02 19:34:42 +0300

    Configuring error handling

--- QuestRooms/Startup.cs
+++ QuestRooms/Startup.cs
@@ -42,6 +42,15 @@ namespace QuestRooms
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsProduction())
+            {
+                app.UseExceptionHandler("/error");
+            }
+            else
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseStatusCodePages();
+            }
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
